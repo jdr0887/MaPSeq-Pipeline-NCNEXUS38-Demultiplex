26dce4931e674166da0ab626eaa0064fb21032ae
Jason Reilly <jdr0887@gmail.com>
2017-01-12 12:13:13 -0500

    renaming

---
+++ ncnexus38-demultiplex-executor/src/main/java/edu/unc/mapseq/executor/ncnexus38/demultiplex/NCNEXUS38DemultiplexWorkflowExecutorService.java
@@ -0,0 +1,50 @@
+package edu.unc.mapseq.executor.ncnexus38.demultiplex;
+
+import java.util.Timer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NCNEXUS38DemultiplexWorkflowExecutorService {
+
+    private static final Logger logger = LoggerFactory.getLogger(NCNEXUS38DemultiplexWorkflowExecutorService.class);
+
+    private final Timer mainTimer = new Timer();
+
+    private NCNEXUS38DemultiplexWorkflowExecutorTask task;
+
+    private Long period = 5L;
+
+    public NCNEXUS38DemultiplexWorkflowExecutorService() {
+        super();
+    }
+
+    public void start() throws Exception {
+        logger.info("ENTERING start()");
+        long delay = 1 * 60 * 1000; // 1 minute
+        mainTimer.scheduleAtFixedRate(task, delay, period * 60 * 1000);
+    }
+
+    public void stop() throws Exception {
+        logger.info("ENTERING stop()");
+        mainTimer.purge();
+        mainTimer.cancel();
+    }
+
+    public NCNEXUS38DemultiplexWorkflowExecutorTask getTask() {
+        return task;
+    }
+
+    public void setTask(NCNEXUS38DemultiplexWorkflowExecutorTask task) {
+        this.task = task;
+    }
+
+    public Long getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(Long period) {
+        this.period = period;
+    }
+
+}
